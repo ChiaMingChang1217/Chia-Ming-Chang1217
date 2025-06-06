ecc60d5251be6fd1a125456df0da427277199db5
ChiaMingChang1217 <changdr1217@gmail.com>
2025-06-06 14:44:19 +0800

    Update grid indepent check.py

--- grid indepent check.py
+++ grid indepent check.py
@@ -8,17 +8,14 @@ Created on Wed Jun  4 14:40:40 2025
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 資料輸入
 Nx = np.array([100, 200, 300, 400, 500])
 Ny = np.array([400, 400, 400, 400, 400])
 err_T = np.array([3.00e-08, 3.43e-09, 1.02e-08, 1.90e-08, 1.53e-08])
 err_D = np.array([0.044614262, 0.017568113, 0.008727451, 0.004338231, 0.001723997])
 err_VM = np.array([8.868971042, 3.615274359, 1.75862114, 0.858331232, 0.337056938])
 
-# 真實網格大小指標
 h = 1.0 / np.sqrt(Nx * Ny)
 
-# 繪圖
 plt.figure(figsize=(6, 4.5))
 plt.loglog(h, err_T, '-o', label="$T_{max}$", lw=2)
 plt.loglog(h, err_D, '-s', label="$disp_{max}$", lw=2)
